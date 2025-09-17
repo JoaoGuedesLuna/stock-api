bc547b677d4e3fe2746d45301d3b4bc7c4734b5e
JoaoGuedesLuna <joaoguedesluna@gmail.com>
2025-09-17 17:58:08 -0300

    :sparkles: feat: add MongooseModule

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { ProductRepository } from '../../repositories/product.repository';
 import { MongooseProductRepository } from '../../repositories/mongoose/mongoose-product.repository';
+import { Product } from './schemas';
+import { ProductSchema } from './schemas/product.shema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }])],
   controllers: [ProductController],
   providers: [
     ProductService,
