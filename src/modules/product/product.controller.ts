e64f825cbc6191a7578233058ead9d464e5ef8cf
JoaoGuedesLuna <joaoguedesluna@gmail.com>
2025-09-17 18:41:49 -0300

    :sparkles: feat: add service and controller

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { CreateProductDto, UpdateProductDto } from './dtos';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productService.create(createProductDto);
+  }
+
+  @Delete(':id')
+  deleteById(@Param('id') id: string) {
+    return this.productService.deleteById(id);
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return this.productService.findById(id);
+  }
+
+  @Put(':id')
+  updateById(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productService.updateById(id, updateProductDto);
+  }
 }
