61acb826120af47cdf5bf60b0018291a4ceeb61d
JoaoGuedesLuna <joaoguedesluna@gmail.com>
2025-09-16 21:05:25 -0300

    :sparkles: feat: set up database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {MongooseModule} from "@nestjs/mongoose";
 
-@Module({})
+@Module({
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                uri: configService.getOrThrow('MONGODB_URI'),
+                dbName: configService.getOrThrow('MONGODB_DB')
+            })
+        })
+    ]
+})
 export class AppModule {}
