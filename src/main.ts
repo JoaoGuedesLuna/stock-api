31dfd2fa086f03f3c19f255dba0357c084aaa1b9
JoaoGuedesLuna <joaoguedesluna@gmail.com>
2025-09-22 12:59:55 -0300

    :sparkles: feat: change API prefix to v1

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { HttpExceptionFilter } from '@/common/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api/v1');
 
   setupSwagger(app);
 
